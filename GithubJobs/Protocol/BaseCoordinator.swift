47acba4616870e89da5f6ba98adb8901b19ebb17
Dev Agani <dev.things.ke@gmail.com>
2021-06-14 07:24:09 +0300

    chore(coordinator-pattern): add reactivity library

---
+++ GithubJobs/Protocol/BaseCoordinator.swift
@@ -0,0 +1,11 @@
+//
+//  BaseCoordinator.swift
+//  GithubJobs
+//
+//  Created by George Nyakundi on 14/06/2021.
+//
+
+import Foundation
+
+
+/// Base abstract coordinator generic over the return type of the `start` method.
